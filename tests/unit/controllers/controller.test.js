d63041f377dfa63711d0651e7554eda63d5a81a4
PedrovRamos <pvramos.cunha@gmail.com>
2023-04-04 01:33:38 -0300

    Adicionando projeto com requisito 7 concluido

--- tests/unit/controllers/controller.test.js
+++ tests/unit/controllers/controller.test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { listProducts, getProduct } = require('../../../src/controllers/product.controller');
 const app = require('../../../src/app')
+const { insertSale } = require('../../../src/controllers/sale.controller')
 
 chai.use(chaiHttp);
 
@@ -18,4 +19,22 @@ describe('Testa se o controller da minha aplicação', () => {
   //   expect(type).to.equal(null)
   //   expect(message).to.be.instanceOf(Object)
   // })
+  it('insere uma venda com sucesso', async () => {
+    const response = await chai
+      .request(app)
+      .post('/sales')
+      .send(
+         [
+            {
+              "productId": 1,
+              "quantity": 1
+            },
+            {
+              "productId": 2,
+              "quantity": 5
+            }
+        ]
+      )
+    chai.expect(response.status).to.be.equal(201);
+  })
 });
--- tests/unit/services/service.test.js
+++ tests/unit/services/service.test.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const { findAll, findById } = require('../../../src/services/product.service');
+const { createSale } = require('../../../src/services/sale.service');
 
 describe('Testa se o service da minha aplicação', () => {
   it('Devolve um erro quando procura por um produto que não existe', async () => {
@@ -13,4 +14,21 @@ describe('Testa se o service da minha aplicação', () => {
     expect(type).to.equal(null)
     expect(message).to.be.instanceOf(Object)
   })
+
+  it('cria uma nova venda com sucesso', async () => {
+    const { message, type } = await createSale(
+      [
+          {
+            "productId": 1,
+            "quantity": 1
+          },
+          {
+            "productId": 2,
+            "quantity": 5
+          }
+      ])
+
+    expect(message).to.be.instanceOf(Object)
+  })
+
 });
